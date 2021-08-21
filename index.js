3a817768c38b8c0d02d450ec116e8ae9544bc78f
rajenderkapil2000 <rajenderkapil2000@gmail.com>
2021-08-21 21:13:10 +0530

    adding logic

--- index.js
+++ index.js
@@ -1,20 +1,37 @@
 const dateOfBirth = document.querySelector("#date-of-birth");
 const luckyNumber = document.querySelector("#lucky-number");
 const checkButton = document.querySelector("#check-button");
+const message = document.querySelector("#message");
 
 
-function checkBirthdateIsLucky(){
-    const dob = dateOfBirth.nodeValue;
-    const sum = calculateSum(dob);
-    console.log(sum)
-}
-
-function calculateSum(dob){
-    dob = dob.replaceAll("-","")
-    let sum = 0;
-    for(let i=0; i< dob.length; i++){
-        
+function compareValues(sum, luckyNumber){
+    if(sum % luckyNumber ===0){
+        message.innerText = "your birthhday is lucky"
+    }else{
+        message.innerText = "your birthhday is not lucky"
     }
 }
 
-checkButton.addEventListener('click',checkBirthdateIsLucky);
+
+function checkBirthdayIsLucky(){
+ const   dob = dateOfBirth.value;
+ const sum = calculateSum(dob);
+ if(dob&&luckyNumber.value){
+compareValues(sum, luckyNumber.value)
+ }else{
+    message.innerText = "Please enter both the fields"
+ }
+}
+
+function calculateSum(dob){
+    dob = dob.replaceAll("-","");
+    let sum = 0;
+    for (let i=0; i<dob.length; i++){
+        sum = sum + Number(dob.charAt(i));
+    }
+    return sum;
+}
+
+
+
+checkButton.addEventListener("click", checkBirthdayIsLucky);
